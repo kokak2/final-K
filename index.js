d74765db0f8e5ff56032ff3c817f61dc103192f7
kokak2 <74504614+kokak2@users.noreply.github.com>
2021-01-08 08:07:31 +0400

    update 01.08.2021

---
+++ index.js
@@ -0,0 +1,23 @@
+function f212(){
+  document.getElementById("s1d021").innerHTML=document.getElementById("s1d212").innerHTML
+  document.getElementById("s1d021").setAttribute('class', 's1-div-212');
+  document.getElementById("s1d021").style.width='420px';
+  document.getElementById("s1d021").style.height='400px';
+};
+function f213(){
+  document.getElementById("s1d021").innerHTML=document.getElementById("s1d213").innerHTML
+  document.getElementById("s1d021").setAttribute('class', 's1-div-213');
+  document.getElementById("s1d021").style.width='420px';
+  document.getElementById("s1d021").style.height='400px';
+};
+function f214(){
+  document.getElementById("s1d021").innerHTML=document.getElementById("s1d214").innerHTML
+  document.getElementById("s1d021").setAttribute('class', 's1-div-214');
+  document.getElementById("s1d021").style.width='420px';
+  document.getElementById("s1d021").style.height='400px';
+};
+function nav(){
+  document.getElementById("hd1").innerHTML=document.getElementById("nav").innerHTML
+  console.log('dfhbsdh')
+  document.getElementById("hd1").setAttribute('class', 'flex-col h-nav2-ul')
+}
